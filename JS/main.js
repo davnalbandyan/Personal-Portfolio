bf9724d2647e9404ca6a11ef64e856124de42b24
davnalbandyan <davnalbandyan@gmail.com>
2023-12-24 10:43:03 +0400

    add event to see the info about

--- JS/main.js
+++ JS/main.js
@@ -1,20 +1,14 @@
 "use strict"
 
 let tabLinks = document.querySelectorAll(".tab-links");
-
 let tabContents = document.querySelectorAll(".tab-contents");
 
-tabLinks.forEach(tab =>{
-    tab.addEventListener("click",function(){
-        tabLinks.forEach(link=>{
-            link.classList.remove("active-link")
-        })
+tabLinks.forEach((tab,index)=>{
+    tab.addEventListener("click",()=>{
+        tabLinks.forEach(tabItem=>tabItem.classList.remove("active-link"));
+        tabContents.forEach(contentItem=>contentItem.classList.remove("active-tab"));
 
-        tabContents.forEach(itemTab=>{
-            itemTab.classList.remove("active-tab")
-        })
+        tabContents[index].classList.add("active-tab");
+        tab.classList.add("active-link")
     })
-
-
 })
-
