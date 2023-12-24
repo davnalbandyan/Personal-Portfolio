6983ab44ee9c690696f59e0458b74ebd595ccda4
davnalbandyan <davnalbandyan@gmail.com>
2023-12-24 18:25:56 +0400

    have change the code of JS part

--- JS/main.js
+++ JS/main.js
@@ -4,11 +4,15 @@ let tabLinks = document.querySelectorAll(".tab-links");
 let tabContents = document.querySelectorAll(".tab-contents");
 
 tabLinks.forEach((tab,index)=>{
-    tab.addEventListener("click",()=>{
-        tabLinks.forEach(tabItem=>tabItem.classList.remove("active-link"));
-        tabContents.forEach(contentItem=>contentItem.classList.remove("active-tab"));
+   tab.addEventListener("click",()=>{
+    tabLinks.forEach(item => item.classList.remove("active-link"));
+    tabContents.forEach(content => content.classList.remove("active-tab"));
 
-        tabContents[index].classList.add("active-tab");
-        tab.classList.add("active-link")
-    })
+    tab.classList.add("active-link");
+    tabContents[index].classList.add("active-tab")
+   })
 })
+
+
+
+
